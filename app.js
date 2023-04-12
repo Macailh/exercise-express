c3ba4f33833e2adf25ed4236dbadfe03d521bb6e
SalvadorGerman <salvador.german@ikusi.com>
2023-04-12 16:22:37 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Express application
+import express from 'express' 
+
+// Configuration
+const cfg = {
+    port: process.env.PORT || 3000
+}
+
+// Express initiation
+const app = express()
+
+// Home page route
+app.get("/", (req, res) => {
+    res.send("Hellow Enn")
+})
+
+// Start server
+app.listen(cfg.port, () => {
+    console.log(`Example app listening at http://localhost:${cfg.port}`)
+})
